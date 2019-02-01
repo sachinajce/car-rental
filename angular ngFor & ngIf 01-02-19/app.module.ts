b75b4939b21a3397fbcb3590b48761d5d179e638
sachinajce <37173798+sachinajce@users.noreply.github.com>
2019-02-01 16:27:26 +0530

    Add files via upload

---
+++ angular ngFor & ngIf 01-02-19/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { IndiaComponent } from './india/india.component';
+import { ProfileComponent } from './profile/profile.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    IndiaComponent,
+    ProfileComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
